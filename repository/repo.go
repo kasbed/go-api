489953cd0885e6df096de32703fe3c8ed2b50317
Alberto <albertom359@gmail.com>
2022-04-04 20:46:02 +0200

    added insert/delete user

--- repository/repo.go
+++ repository/repo.go
@@ -3,7 +3,10 @@ package repository
 import (
 	"context"
 	"log"
+	"src/structs"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -12,7 +15,7 @@ var collection *mongo.Collection
 var ctx = context.TODO()
 
 
-func connect() {
+func connect() * mongo.Client {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil { log.Fatal(err) }
@@ -20,4 +23,23 @@ func connect() {
 	if err != nil { log.Fatal(err) }
 
 	collection = client.Database("goApi").Collection("goApi")
+
+	return client
 }
+
+func insertUser(user *structs.UserDTO) *structs.UserDTO {
+	//Convert dto to data and insert in mongo
+	client := connect();
+	collection = client.Database("goApi").Collection("user")
+	data := structs.User{ID: primitive.NewObjectID(), CreatedAt: time.Now(), UpdatedAt: time.Now(), 
+		UserName: user.UserName, Email: user.Email, FirstName: user.FirstName, SurName: user.SurName}
+	collection.InsertOne(ctx, data)
+	return user;
+}
+
+func removeUser(id *primitive.ObjectID) {
+	client := connect();
+	collection = client.Database("goApi").Collection("user")
+	collection.DeleteOne(ctx, id)
+}
+
